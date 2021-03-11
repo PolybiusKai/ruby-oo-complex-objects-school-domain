11cf83b3b3e6390dfd8fee6ca18885e1b7aaea2c
rsmith <legendary.belief@gmail.com>
2021-03-11 15:15:08 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+class School
+    attr_accessor :roster, :student, :grade
+
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if @roster[grade]
+          @roster[grade] << student
+        else
+          @roster[grade] = []
+          @roster[grade] << student
+        end
+      end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort 
+        new_list = {}
+        @roster.each {|k, v| new_list[k] = v.sort}
+        new_list
+    end
+
+    
+end
