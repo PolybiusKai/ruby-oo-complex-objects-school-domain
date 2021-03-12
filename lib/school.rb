7fa1f90dfef37a2db8445b3b757f3f08c06b1471
rsmith <legendary.belief@gmail.com>
2021-03-12 12:53:39 -0800

    commit tree 40ec549db0fc39d9196aded04c5c1944faa01120

--- lib/school.rb
+++ lib/school.rb
@@ -1,28 +1,23 @@
-# code here!
 
 class School
-    attr_accessor :roster, :student, :grade
+  attr_accessor :roster, :student, :grade
 
-    def initialize(roster)
-        @roster = {}
-    end
+  def initialize(roster)
+      @roster = {}
+  end
 
-    def add_student(student, grade)
-        if @roster[grade]
-          @roster[grade] << student
-        else
-          @roster[grade] = []
-          @roster[grade] << student
-        end
-      end
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
 
-    def grade(grade)
-        @roster[grade]
-    end
+  def grade(grade)
+      @roster[grade]
+  end
 
-    def sort 
-        new_list = {}
-        @roster.each {|k, v| new_list[k] = v.sort}
-        new_list
-    end
+  def sort 
+      new_list = {}
+      @roster.each {|k, v| new_list[k] = v.sort}
+      new_list
+  end
 end
